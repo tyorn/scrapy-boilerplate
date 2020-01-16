7a706b76880c50309b733597f50b442b23ef4c72
gusv_aa <gusev_aa@groupbwt.com>
2020-01-16 13:14:42 +0200

    add one to many test spider

---
+++ src/spiders/test/one_to_many.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+
+import scrapy
+
+from helpers import RMQObject
+
+
+class OneToManyTestSpider(scrapy.Spider):
+    name = 'one-many-test'
+
+    custom_settings = {
+        'EXTENSIONS': {
+            'extensions.PikaBaseConsumer': 21,
+        },
+        'ITEM_PIPELINES': {
+            'pipelines.pika_test_pipeline.PikaTestPipeline': 500,
+        }
+    }
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.rmq_settings = dict()
+        self.rmq_settings['queue'] = 'test_queue'
+        self.rmq_settings['create_request_callback'] = self.__create_request
+
+    def __create_request(self, message: dict, rmq_object: RMQObject):
+        return scrapy.Request(
+            url=message['url'],
+            callback=self.parse,
+            errback=self.parse,
+            meta={
+                'rmq_object': rmq_object
+            },
+            dont_filter=True,
+        )
+
+    def parse(self, response):
+        for i in range(2):
+            yield {
+                'id': i,
+                'datetime': datetime.now(),
+                'ip': response.css('#ip::text').get().strip()
+            }
+
+        retry = response.meta.get('retry', 0)
+        if retry < 2:
+            meta = response.meta
+            meta.update({'retry': retry + 1})
+            yield scrapy.Request(
+                response.url,
+                self.parse,
+                dont_filter=True,
+                meta=meta
+            )
+        else:
+            response.meta['rmq_object'].ack()
+
